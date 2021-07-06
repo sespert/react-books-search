1222678ad1a4c3864425d4cca1f73840b63cb0a5
sespertg <sespert@gmail.com>
2021-07-06 14:03:23 -0400

    added word nice to test private repo

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Jumbotron () {
     return (
         <div className="jumbotron" style={{ height: 300, textAlign: "center"}}>
-            <h1>Google Books Search</h1>
+            <h1>Nice Google Books Search</h1>
             <hr className="my-4"/>
             <h4>Search for books in the Google Books database, then save your favorites to a list to be displayed whenever you need them</h4>
         </div>
